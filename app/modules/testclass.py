fd0fbd6f8dc717a77a44765ce93f9c982adf9271
Sam Sun <ssun@marinsoftware.com>
2017-08-31 17:24:36 +0800

    Implement TestClasser to parse java code

--- app/modules/testclass.py
+++ app/modules/testclass.py
@@ -4,6 +4,7 @@ __author__    = "Copyright (c) 2017, Marin Software"
 __copyright__ = "Licensed under GPLv2 or later."
 
 import pprint
+import javalang
 
 """ Parser of a Java test class powered by JUnit
     including the module, class name, test methods, test cases, test JSON files etc.
@@ -17,15 +18,45 @@ class TestClasser:
         self.load()
 
     def load(self):
-        pass
+        file = open(self.javaFile,'r').read()
+        tree = javalang.parse.parse(file)
+        className = tree.types[0].name
+        testMethods = []
+        for testMethod in tree.types[0].body:
+            method = {}
+            for testAnnotation in testMethod.annotations:
+                if testAnnotation.name == 'Test':
+                    method['name'] = testMethod.name
+                    method['className'] = className
+                if testAnnotation.name == 'FileParameters':
+                    for parameter in testAnnotation.element:
+                        if parameter.name == 'value':
+                            method['testJson'] = parameter.value.value.replace('"','')
+                            break
+                    break
+                if testAnnotation.name == 'Parameters':
+                    for parameter in testAnnotation.element:
+                        if parameter.name == 'method':
+                            method['method'] = parameter.value.value.replace('"','')
+                            break
+                    break
+            if method:
+                testMethods.append(method)
+        return testMethods
+
+
 
 
 
 if (__name__ == '__main__'):
 
-    testFile = '/Users/mxu/Workspace/qe/int/tests/qe-metadata-service-tests/src/test/java/com/marin/qa/metadata/CustomerTest.java'
+    testFile = '/Users/ssun/git/qe/int/tests/qe-metadata-service-tests/src/test/java/com/marin/qa/metadata/CustomerTest.java'
+    #testFile = '/Users/ssun/git/qe/int/tests/qe-revenue-upload-validator-service-tests/src/test/java/com/marin/qa/revenueuploadvalidatorservice/BingRevenueUploadValidatorServiceTest.java'
     testClasser = TestClasser(testFile)
     pprint.pprint(testClasser.load())
+
+
+
 
 
 
