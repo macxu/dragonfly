42aa43337b198c72df1cb5f41dbd3519047cfacd
Mac Xu <mxu@marinsoftware.com>
2017-09-07 13:36:20 +0800

    introduce blueprints for better code organization.

---
+++ app/modules/jenkins/api.py
@@ -0,0 +1,63 @@
+
+"""Module for Jenkins data parsing"""
+from app.modules.jenkins.jenkins import Jenkins
+from app.modules.jenkins.jenkinsJobReporter import JenkinsJobReporter
+from app.modules.rester import Rester
+
+__author__    = "Copyright (c) 2017, Marin Software>"
+__copyright__ = "Licensed under GPLv2 or later."
+
+
+from flask import Blueprint, request, jsonify
+
+jenkinsAPI = Blueprint('api', 'api', url_prefix='/api/jenkins')
+
+
+@jenkinsAPI.route('/build', methods=['GET'])
+# retrieves/adds polls from/to the database
+def getBuildData():
+    if (not request.args.get('build')):
+        return jsonify({})
+
+    jenkinsBuildUrl = request.args.get('build')
+    jenkins = Jenkins()
+    jobUrl = jenkins.getJobByBuild(jenkinsBuildUrl)
+
+    reporter = JenkinsJobReporter(jobUrl)
+    reporter.load()
+    report = reporter.getReport()
+
+    return jsonify(report)
+
+
+# http://127.0.0.1:5000/jenkins/view/jobs?view=http://ci.marinsw.net/view/Qe/view/Release/view/release-011/view/Tests/
+@jenkinsAPI.route('/jobs', methods=['GET'])
+def getJobsByView():
+    viewUrl = request.args['view']
+    if (not viewUrl):
+        return jsonify({"error": "missing query arg of 'view'!"})
+
+    jenkins = Jenkins()
+    jobs = jenkins.getJobsOfView(viewUrl)
+
+    return jsonify(jobs)
+
+
+@jenkinsAPI.route('/releases')
+def getJenkinsReleaseData():
+
+    if (not request.args.get('release')):
+        return jsonify({})
+
+    releaseNumber = request.args.get('release')
+    viewUrl = 'http://ci.marinsw.net/view/Qe/view/Release/view/' + releaseNumber + '/view/Tests/'
+    print("view URL: " + viewUrl)
+
+    reports = []
+
+    jenkins = Jenkins()
+    reporters = jenkins.getReportersByView(viewUrl)
+    for reporter in reporters:
+        reports.append(reporter.getReport())
+
+    return jsonify(reports)
--- app/run.py
+++ app/run.py
@@ -8,6 +8,10 @@ from app.modules.mongo import Mongo
 app = Flask(__name__)
 mongo = Mongo(app)
 
+from app.modules.jenkins.api import jenkinsAPI
+
+app.register_blueprint(jenkinsAPI)
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -37,59 +41,16 @@ def jobHistory():
     return render_template("job_history.html")
 
 
-# http://127.0.0.1:5000/jenkins/view/jobs?view=http://ci.marinsw.net/view/Qe/view/Release/view/release-011/view/Tests/
-@app.route('/api/jenkins/jobs')
-def getJobsByView():
-    viewUrl = request.args['view']
-    if (not viewUrl):
-        return jsonify({"error": "missing query arg of 'view'!"})
-
-    jenkins = Jenkins()
-    jobs = jenkins.getJobsOfView(viewUrl)
-
-    return jsonify(jobs)
-
-
 @app.route('/api/mongo/releases_stats')
 def getJenkinsReleaseStats():
 
     stats = mongo.getReleasesStats()
     return jsonify(stats)
 
-@app.route('/api/jenkins/build')
-def getJenkinsBuildData():
-    if (not request.args.get('build')):
-        return jsonify({})
-
-    jenkinsBuildUrl = request.args.get('build')
-    jenkins = Jenkins()
-    jobUrl = jenkins.getJobByBuild(jenkinsBuildUrl)
-
-    reporter = JenkinsJobReporter(jobUrl)
-    reporter.load()
-    report = reporter.getReport()
-
-    return jsonify(report)
 
 
-@app.route('/api/jenkins/releases')
-def getJenkinsReleaseData():
 
-    if (not request.args.get('release')):
-        return jsonify({})
 
-    releaseNumber = request.args.get('release')
-    viewUrl = 'http://ci.marinsw.net/view/Qe/view/Release/view/' + releaseNumber + '/view/Tests/'
-    print("view URL: " + viewUrl)
-
-    reports = []
-
-    jenkins = Jenkins()
-    reporters = jenkins.getReportersByView(viewUrl)
-    for reporter in reporters:
-        reports.append(reporter.getReport())
-
-    return jsonify(reports)
 
 
 # http://127.0.0.1:5000/jenkins/view/jobs?view=http://ci.marinsw.net/view/Qe/view/Release/view/release-011/view/Tests/
