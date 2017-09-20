eca3b160b847bb32276f08c30afc2d7b1231de93
Mac Xu <mxu@marinsoftware.com>
2017-09-20 11:18:55 +0800

    use the cross-line string for sql.

--- app/modules/mysql.py
+++ app/modules/mysql.py
@@ -50,62 +50,71 @@ class MysqlClient:
 
     def queryDmtCampaignDiscrepancy(self, clientIds='12654910'):
 
-        sql = ""
-        sql += "SELECT pa.client_id, ";
-        sql += "pa.publisher_id, ";
-        sql += "pc.publisher_campaign_status AS status, ";
-        sql += "pc.publisher_campaign_operation_status AS operational_status, ";
-        sql += "COUNT(*) AS campaign_count, ";
-        sql += "COUNT(CONCAT( ";
-        sql += " CASE WHEN pa.publisher_id = 4 THEN ";
-        sql += "  google.client_customer_id ";
-        sql += "  WHEN pa.publisher_id = 6 THEN ";
-        sql += "  msn.account_id ";
-        sql += "ELSE ";
-        sql += "pca.alias ";
-        sql += "END, '|', pc.ext_id, '-')) AS combined_ext_id_count_total, ";
-        sql += "COUNT(DISTINCT concat( ";
-        sql += " CASE WHEN pa.publisher_id = 4 THEN ";
-        sql += "    google.client_customer_id ";
-        sql += "  WHEN pa.publisher_id = 6 THEN ";
-        sql += "    msn.account_id ";
-        sql += "  ELSE ";
-        sql += "    pca.alias ";
-        sql += " END, '|', pc.ext_id, '-')) AS combined_ext_id_count_distinct, ";
-        sql += "COUNT(pc.publisher_campaign_id) AS legacy_id_count_total, ";
-        sql += "SUM(IF(pc.publisher_campaign_id IS null, 1, 0)) AS legacy_id_count_null, ";
-        sql += "COUNT(DISTINCT pc.publisher_campaign_id) AS legacy_id_count_distinct, ";
-        sql += "COUNT(pc.publisher_campaign_id) AS id_count_total, ";
-        sql += "SUM(IF(pc.publisher_campaign_id IS null, 1, 0)) AS id_count_null, ";
-        sql += "COUNT(DISTINCT pc.publisher_campaign_id) AS id_count_distinct, ";
-        sql += "COUNT(DISTINCT ";
-        sql += "  CASE WHEN pc.ext_id IS NULL THEN ";
-        sql += "    pc.publisher_campaign_id ";
-        sql += "  END) AS ext_id_count_null, ";
-        sql += "SUM(daily_budget) AS budget_sum ";
-        sql += "FROM publisher_accounts pa ";
-        sql += "JOIN publisher_client_accounts pca ON pa.publisher_account_id = pca.publisher_account_id ";
-        sql += "JOIN publisher_campaigns pc ON pca.client_account_id = pc.client_account_id ";
-        sql += "LEFT JOIN google_client_accounts google ON pca.client_account_id = google.google_client_account_id ";
-        sql += "LEFT JOIN msn_client_accounts msn ON pca.client_account_id = msn.msn_client_account_id ";
-        sql += "WHERE pa.client_id IN (12654910) ";
-        sql += "AND pa.publisher_id IN (4,6) ";
-        sql += "GROUP BY pa.client_id, ";
-        sql += "pa.publisher_id, ";
-        sql += "pc.publisher_campaign_status,";
-        sql += "pc.publisher_campaign_operation_status;";
+        sql = """
+        
+        SELECT *
+        FROM publisher_campaigns
+        LIMIT 3
+        
+        
+        """
+
+        # sql = ""
+        # sql += "SELECT pa.client_id, ";
+        # sql += "pa.publisher_id, ";
+        # sql += "pc.publisher_campaign_status AS status, ";
+        # sql += "pc.publisher_campaign_operation_status AS operational_status, ";
+        # sql += "COUNT(*) AS campaign_count, ";
+        # sql += "COUNT(CONCAT( ";
+        # sql += " CASE WHEN pa.publisher_id = 4 THEN ";
+        # sql += "  google.client_customer_id ";
+        # sql += "  WHEN pa.publisher_id = 6 THEN ";
+        # sql += "  msn.account_id ";
+        # sql += "ELSE ";
+        # sql += "pca.alias ";
+        # sql += "END, '|', pc.ext_id, '-')) AS combined_ext_id_count_total, ";
+        # sql += "COUNT(DISTINCT concat( ";
+        # sql += " CASE WHEN pa.publisher_id = 4 THEN ";
+        # sql += "    google.client_customer_id ";
+        # sql += "  WHEN pa.publisher_id = 6 THEN ";
+        # sql += "    msn.account_id ";
+        # sql += "  ELSE ";
+        # sql += "    pca.alias ";
+        # sql += " END, '|', pc.ext_id, '-')) AS combined_ext_id_count_distinct, ";
+        # sql += "COUNT(pc.publisher_campaign_id) AS legacy_id_count_total, ";
+        # sql += "SUM(IF(pc.publisher_campaign_id IS null, 1, 0)) AS legacy_id_count_null, ";
+        # sql += "COUNT(DISTINCT pc.publisher_campaign_id) AS legacy_id_count_distinct, ";
+        # sql += "COUNT(pc.publisher_campaign_id) AS id_count_total, ";
+        # sql += "SUM(IF(pc.publisher_campaign_id IS null, 1, 0)) AS id_count_null, ";
+        # sql += "COUNT(DISTINCT pc.publisher_campaign_id) AS id_count_distinct, ";
+        # sql += "COUNT(DISTINCT ";
+        # sql += "  CASE WHEN pc.ext_id IS NULL THEN ";
+        # sql += "    pc.publisher_campaign_id ";
+        # sql += "  END) AS ext_id_count_null, ";
+        # sql += "SUM(daily_budget) AS budget_sum ";
+        # sql += "FROM publisher_accounts pa ";
+        # sql += "JOIN publisher_client_accounts pca ON pa.publisher_account_id = pca.publisher_account_id ";
+        # sql += "JOIN publisher_campaigns pc ON pca.client_account_id = pc.client_account_id ";
+        # sql += "LEFT JOIN google_client_accounts google ON pca.client_account_id = google.google_client_account_id ";
+        # sql += "LEFT JOIN msn_client_accounts msn ON pca.client_account_id = msn.msn_client_account_id ";
+        # sql += "WHERE pa.client_id IN (12654910) ";
+        # sql += "AND pa.publisher_id IN (4,6) ";
+        # sql += "GROUP BY pa.client_id, ";
+        # sql += "pa.publisher_id, ";
+        # sql += "pc.publisher_campaign_status,";
+        # sql += "pc.publisher_campaign_operation_status;";
 
         return self.query(self)
 
 if (__name__ == '__main__'):
 
     mysqlClient = MysqlClient()
-    # results = mysqlClient.query("SELECT * from publisher_campaigns LIMIT 2")
-    #
-    # pprint(results)
+    results = mysqlClient.query("SELECT * from publisher_campaigns LIMIT 2")
 
-    results = mysqlClient.queryDmtCampaignDiscrepancy()
     pprint(results)
+
+    # results = mysqlClient.queryDmtCampaignDiscrepancy()
+    # pprint(results)
 
     mysqlClient.close()
 
