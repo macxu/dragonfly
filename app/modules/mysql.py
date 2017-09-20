520edd3ed104008d39730e8021b396c2ae14e715
Mac Xu <mxu@marinsoftware.com>
2017-09-20 10:10:12 +0800

    get mysql connection work.

---
+++ app/modules/mysql.py
@@ -0,0 +1,53 @@
+"""Module for MySql Client"""
+
+__author__    = "Copyright (c) 2017, Marin Software>"
+__copyright__ = "Licensed under GPLv2 or later."
+
+# need to install module: Flask-MySQLdb===0.2.0
+from flaskext.mysql import MySQL
+from flask import Flask
+
+class MysqlClient:
+
+    def __init__(self, flaskApp=None):
+
+        self.mysql = MySQL()
+
+        if (flaskApp == None):
+            flaskApp = Flask(__name__)
+
+        flaskApp.config['MYSQL_DATABASE_USER'] = 'XXXXX'
+        flaskApp.config['MYSQL_DATABASE_PASSWORD'] = 'XXXXXXXX'
+        flaskApp.config['MYSQL_DATABASE_DB'] = 'marin'
+        flaskApp.config['MYSQL_DATABASE_HOST'] = 'dbp105.prod.marinsw.net'
+
+        self.mysql.init_app(flaskApp)
+
+        self.conn = self.mysql.connect()
+        cursor = self.conn.cursor()
+
+        cursor.execute("SELECT * from publisher_campaigns LIMIT 2")
+        fetchResult = cursor.fetchall()
+
+        data = str(fetchResult)
+
+        self.conn.close()
+
+        sdf= 0
+
+
+
+    """ Get the list of the test project names in the
+    """
+    def connect(self):
+        pass
+        # from flask_mysqldb import MySQL
+
+
+
+
+if (__name__ == '__main__'):
+
+    mysqlClient = MysqlClient()
+    mysqlClient.connect()
+
